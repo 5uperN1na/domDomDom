25b677976509d47bf4f49c077116858846a3b515
Paula Suarez <pauladzuck@hotmail.com>
2018-08-21 05:40:27 -0500

    Added moveover and mouseleave events to scripts file.

--- scripts.js
+++ scripts.js
@@ -12,17 +12,59 @@ window.addEventListener('DOMContentLoaded', function () {
 
     let i = 0;
     function insertSquare() {
-        let div = document.createElement(div);
+        let div = document.createElement('div');
         let divText = document.createTextNode('Div ID:' + ' ' + i);
-        div.appendChild(divText);
+        //div.appendChild(divText);
         div.className = 'div';
         divContainer.appendChild(div);
         div.setAttribute('i', i);
-        document.body.appendChild(divContainer);
+
         i++;
+
+        //Created event listener when each div is clicked, calls getRandom (color) function.
+        div.addEventListener("click", function () {
+            div.style.backgroundColor = getRandom();
+        });
+
+        div.addEventListener('mouseover', function () {
+            div.appendChild(divText);
+    
+        });
+    
+        div.addEventListener('mouseleave', function () {
+            divText.remove();
+        });
+
+
+
 
     }
 
+    //document.body.appendChild(div);
+
     button.addEventListener('click', insertSquare);
+
+    //create an array with random colors
+    let colors = [
+        'pink',
+        'blue',
+        'orange',
+        'black',
+        'green',
+        'brown',
+        'yellow',
+        'red',
+    ];
+
+    //created a function that take color array and randomly assign color.
+    function getRandom() {
+        let random = colors[Math.floor(Math.random() * colors.length)];
+        return random;
+
+    }
+
+
+
+
 
 });
